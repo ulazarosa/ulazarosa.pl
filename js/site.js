dbe6d49efa755f79500de57b7f3be090045a620e
Kazik Pogoda <morisil@xemantic.com>
2014-08-13 01:00:19 +0200

    scroll to java script

---
+++ js/site.js
@@ -0,0 +1,13 @@
+$(document).ready(function() {
+
+  // initialize scrollto
+  $('a.scrollto').click(function(e){
+    $('html,body').scrollTo(this.hash, this.hash, {gap:{y: 0}});
+    e.preventDefault();
+
+    if ($('.navbar-collapse').hasClass('in')){
+      $('.navbar-collapse').removeClass('in').addClass('collapse');
+    }
+  });
+
+});
