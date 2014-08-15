112c7478ccfa9c3f9d777a2412143e82303e6e59
Kazik Pogoda <morisil@xemantic.com>
2014-08-15 20:38:48 +0200

    adjust scroll to scrolling to header offset

--- js/site.js
+++ js/site.js
@@ -2,7 +2,7 @@ $(document).ready(function() {
 
   // initialize scrollto
   $('a.scrollto').click(function(e){
-    $('html,body').scrollTo(this.hash, this.hash, {gap:{y: 0}});
+    $('html,body').scrollTo(this.hash, this.hash, {gap:{y: -50}});
     e.preventDefault();
 
     if ($('.navbar-collapse').hasClass('in')){
